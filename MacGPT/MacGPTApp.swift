d94bc1b841f12349da14deb7cac8e34b49d5f2f0
AmirHossein Teymoori <teymoori.net@gmail.com>
2023-11-18 00:07:29 +0100

    Initial Commit

---
+++ MacGPT/MacGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacGPTApp.swift
+//  MacGPT
+//
+//  Created by Amirhossein Teymoori on 2023-11-18.
+//
+
+import SwiftUI
+
+@main
+struct MacGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
