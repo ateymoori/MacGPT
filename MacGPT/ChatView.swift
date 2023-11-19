2def62f950a2a92be71d22e309cc9e713211f9a3
AmirHossein Teymoori <teymoori.net@gmail.com>
2023-11-19 17:50:32 +0100

    some fixed in prompts

--- MacGPT/ChatView.swift
+++ MacGPT/ChatView.swift
@@ -12,7 +12,8 @@ struct ChatView: View {
     @State private var outputText: String = ""
     let characterLimit = 1500
     @State private var isLoading: Bool = false
-    
+    @State private var selectedMode: String = "Rephrase"
+    let modes = ["Rephrase", "Question"]
     
     var body: some View {
             VStack {
@@ -31,6 +32,18 @@ struct ChatView: View {
                             .buttonStyle(BorderlessButtonStyle())
                         }
                         .padding([.top, .horizontal])
+                
+                
+                
+                // Mode Selection (Rephrase / Question)
+                   Picker("Mode", selection: $selectedMode) {
+                       ForEach(modes, id: \.self) {
+                           Text($0)
+                       }
+                   }
+                   .pickerStyle(SegmentedPickerStyle())
+                   .padding()
+
                 
                 
                 // Input TextEditor
@@ -107,8 +120,16 @@ struct ChatView: View {
  
     func askChatGPT() {
         isLoading = true
-        let settingsModel = SettingsModel()
-        OpenAIManager.shared.askQuestion(prompt: sharedTextModel.inputText, systemMessage: settingsModel.initialPrompt) { result in
+
+        
+        let systemMessage: String
+        if selectedMode == "Rephrase" {
+            systemMessage = "As an AI proficient in Swedish, English, and Persian, your task is to identify the language of any phrase I provide. If the phrase is in Swedish or Persian, translate it into English and respond only with the English translation. Refrain from adding any extra commentary. If the phrase is in English, review its spelling, grammar, and tone. Your objective is to correct any grammatical or spelling errors, while preserving the original meaning, logic, and tone of the phrase. Provide the corrected version as your response"
+        } else { // "Question" mode
+            systemMessage = "As an AI assistant equipped with knowledge of modern trends and extensive data, your role is to respond to my queries with clarity, directness, and conciseness. Provide answers that are straightforward and detailed, yet brief, avoiding any extraneous information. Just focus on addressing the question at hand. Before responding, identify the language of my question and use the same language for your answer"
+        }
+        
+        OpenAIManager.shared.askQuestion(prompt: sharedTextModel.inputText, systemMessage: systemMessage) { result in
             DispatchQueue.main.async {
                 isLoading = false
                 switch result {
