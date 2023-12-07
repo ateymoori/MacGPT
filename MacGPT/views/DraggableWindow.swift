33de9a5282f386cd3b2f9b9fd2b6ff84ac85c93f
Amir <amirhossein.teymoori@sbab.se>
2023-12-07 17:26:10 +0100

    texts are coming from API

--- MacGPT/views/DraggableWindow.swift
+++ MacGPT/views/DraggableWindow.swift
@@ -7,25 +7,39 @@
 
 import Foundation
 import Cocoa
-
 class DraggableWindow: NSWindow {
     private var initialLocation: NSPoint?
+    private var dragging = false
 
     override func mouseDown(with event: NSEvent) {
-        initialLocation = event.locationInWindow
+        let location = event.locationInWindow
+        initialLocation = location
+        dragging = false
+        super.mouseDown(with: event) // Allow the event to propagate
     }
 
     override func mouseDragged(with event: NSEvent) {
-        guard let initialLocation = initialLocation else { return }
+        if let initialLocation = initialLocation, !dragging {
+            let currentLocation = event.locationInWindow
+            let deltaX = currentLocation.x - initialLocation.x
+            let deltaY = currentLocation.y - initialLocation.y
 
-        let currentLocation = event.locationInWindow
-        let deltaX = currentLocation.x - initialLocation.x
-        let deltaY = currentLocation.y - initialLocation.y
+            var newOrigin = frame.origin
+            newOrigin.x += deltaX
+            newOrigin.y += deltaY
 
-        var newOrigin = frame.origin
-        newOrigin.x += deltaX
-        newOrigin.y += deltaY
+            setFrameOrigin(newOrigin)
+            dragging = true
+        } else {
+            super.mouseDragged(with: event) // Allow the event to propagate
+        }
+    }
 
-        setFrameOrigin(newOrigin)
+    override func mouseUp(with event: NSEvent) {
+        if dragging {
+            dragging = false
+        } else {
+            super.mouseUp(with: event) // Allow the event to propagate
+        }
     }
 }
--- MacGPT/views/ReminderView.swift .swift
+++ MacGPT/views/ReminderView.swift .swift
@@ -6,68 +6,131 @@
 //
 
 import SwiftUI
- 
 import AVFoundation
 
-
 struct ReminderView: View {
-    // Extracted data from the CSV file
-    let phrases = [
-        ("God morgon", "Good morning"),
-        ("Hej", "Hello"),
-        ("Hej då", "Goodbye"),
-        ("Vi ses", "See you"),
-        ("Ta hand om dig", "Take care"),
-        ("Trevlig helg", "Have a nice weekend"),
-        ("Vilken vacker dag", "What a beautiful day"),
-        ("God natt", "Good night"),
-        ("Tack och hej", "Thanks and bye"),
-        ("Vi ses imorgon", "See you tomorrow"),
-        ("Ha en bra dag", "Have a good day"),
-        ("Lycka till", "Good luck"),
-        ("Välkommen", "Welcome"),
-        ("Trevligt att träffas", "Nice to meet you"),
-        ("Ha det så bra", "Take care (informal)"),
-        ("Tack för idag", "Thanks for today"),
-        ("Vi hörs", "Talk to you later"),
-        ("Ha en trevlig kväll", "Have a nice evening"),
-        ("God helg", "Have a good weekend"),
-        ("Ha en bra semester", "Have a good vacation")
-    ]
-    
+    @State private var userInput: String = ""
+    @State private var phrases: [(String, String)] = []
+
+    init() {
+         printAvailableVoices()
+        self.speak(text : "Amir Amir")
+         // Other initialization code if necessary
+     }
+    private func printAvailableVoices() {
+           let availableVoices = AVSpeechSynthesisVoice.speechVoices().filter { $0.language == "sv-SE" }
+           print("Available Swedish Voices: \(availableVoices)")
+       }
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 20) { // Increased spacing between lines
-                ForEach(phrases, id: \.0) { (swedish, english) in
-                    HStack {
-                        Button(action: {
-                            self.speak(text: swedish)
-                        }) {
-                            Image(systemName: "speaker.wave.2.fill")
-                                .foregroundColor(.white)
-                                .accessibility(label: Text("Speak Swedish"))
-                        }
-                        Text(swedish)
-                            .font(.system(size: 18))
-                            .foregroundColor(.white)
-                        Spacer()
-                        Text(english)
-                            .font(.system(size: 18))
-                            .foregroundColor(.white)
+        VStack {
+            
+            Rectangle()
+                .fill(Color.clear)
+                .frame(height: 24)
+            
+            // TextEditor for user input with styled background
+            ZStack(alignment: .topLeading) {
+                TextEditor(text: $userInput)
+                    .opacity(userInput.isEmpty ? 0.5 : 1)
+                    .padding(4)
+                    .background(Color.gray.opacity(0.2)) // Grey background
+                    .cornerRadius(8)
+                    .border(Color.gray, width: 1)
+                
+                if userInput.isEmpty {
+                    Text("Enter your question here")
+                        .foregroundColor(.white)
+                        .padding(.all, 8)
+                        .background(Color.clear)
+                }
+            }
+            .frame(height: 100)
+            .foregroundColor(.white) // Set text color to white
+            
+            // Button to send the question
+            Button("Ask ChatGPT") {
+                fetchPhrasesFromOpenAI(with: userInput)
+            }
+            .padding()
+            
+            // Display the phrases
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20) {
+                    ForEach(phrases, id: \.0) { (swedish, english) in
+                        phraseRow(swedish: swedish, english: english)
                     }
                 }
             }
             .padding()
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color.black.opacity(0.75)) // 70% transparency
-            .cornerRadius(10) // Rounded corners
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black.opacity(0.75)) // 70% transparency
+        .cornerRadius(10) // Rounded corners
         .padding(.horizontal) // Padding on the sides if needed
     }
     
-    private let speechSynthesizer = AVSpeechSynthesizer()
+    func fetchPhrasesFromOpenAI(with userQuery: String) {
+        let systemMessage = """
+        Return a JSON array with 15 common Swedish words related to food along with their English translations. Format the response as follows:
+        [
+            {"Swedish": "SwedishWord1", "English": "EnglishTranslation1"},
+            {"Swedish": "SwedishWord2", "English": "EnglishTranslation2"},
+            ...
+        ]
+        """
 
-    // Function to handle speech
+        OpenAIManager.shared.askQuestion(model: "gpt-3.5-turbo", prompt: userQuery, systemMessage: systemMessage) { result in
+            switch result {
+            case .success(let response):
+                print("Response: \(response)")
+                DispatchQueue.main.async {
+                    self.phrases = parseResponse(response)
+                }
+            case .failure(let error):
+                print("Error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    private func parseResponse(_ response: String) -> [(String, String)] {
+        guard let data = response.data(using: .utf8) else {
+            print("Error: Can't convert string to Data")
+            return []
+        }
+
+        do {
+            let phrasePairs = try JSONDecoder().decode([PhrasePair].self, from: data)
+            return phrasePairs.map { ($0.Swedish, $0.English) }
+        } catch {
+            print("Error parsing JSON: \(error)")
+            return []
+        }
+    }
+
+    
+    // Row view for each phrase
+    @ViewBuilder
+     private func phraseRow(swedish: String, english: String) -> some View {
+         HStack {
+             Button(action: {
+                 self.speak(text: swedish)
+             }) {
+                 Image(systemName: "speaker.wave.2.fill")
+                     .foregroundColor(.white)
+                     .accessibility(label: Text("Speak Swedish"))
+             }
+             Text(swedish)
+                 .font(.system(size: 18))
+                 .foregroundColor(.white)
+             Spacer()
+             Text(english)
+                 .font(.system(size: 18))
+                 .foregroundColor(.white)
+         }
+     }
+    
+    // Speech synthesizer code
+    private let speechSynthesizer = AVSpeechSynthesizer()
     func speak(text: String) {
         let utterance = AVSpeechUtterance(string: text)
         utterance.voice = AVSpeechSynthesisVoice(language: "sv-SE") // Swedish language
@@ -76,4 +139,10 @@ struct ReminderView: View {
         speechSynthesizer.stopSpeaking(at: .immediate)
         speechSynthesizer.speak(utterance)
     }
+
+    struct PhrasePair: Codable {
+        let Swedish: String
+        let English: String
+    }
+    
 }
