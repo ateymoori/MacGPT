1cfae93f3d7ce85206b97b1096381920d5853929
AmirHossein Teymoori <teymoori.net@gmail.com>
2023-12-21 09:50:18 +0100

    code cleandup

--- MacGPT/AppDelegate.swift
+++ MacGPT/AppDelegate.swift
@@ -11,53 +11,66 @@ import Carbon.HIToolbox
 
 
 class AppDelegate: NSObject, NSApplicationDelegate {
+    private enum Constants {
+        static let popoverSize = NSSize(width: 500, height: 400)
+        static let statusBarIconName = "icon"
+        static let hotkeyId = 1
+        static let hotkeyKey = UInt32(kVK_ANSI_6)
+        static let hotkeyModifiers = UInt32(shiftKey | cmdKey)
+    }
+
     var popover: NSPopover!
     var statusBarItem: NSStatusItem!
     var sharedTextModel = SharedTextModel.shared
-    var hotKeyRef: EventHotKeyRef?
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        // Initialize and configure the popover
-        popover = NSPopover()
-        popover.contentSize = NSSize(width: 500, height: 400)
-        popover.behavior = .transient
-        popover.contentViewController = NSHostingController(rootView: ChatView().environmentObject(sharedTextModel)) // Modify this line
 
-        // Create the status bar item
-        statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        if let button = statusBarItem.button {
-            button.image = NSImage(named: "icon") // Ensure this icon exists in your assets
-            button.action = #selector(toggleChatPopover(_:))
-        }
-        
-        // Register services, permissions, and hotkeys
-        registerServices()
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        configurePopover()
+        configureStatusBarItem()
+        registerServicesAndHotkeys()
     }
 
-    
-    private func registerServices() {
-        // Register the service
+    private func configurePopover() {
+        popover = NSPopover()
+        popover.contentSize = Constants.popoverSize
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: ChatView().environmentObject(sharedTextModel))
+    }
+
+    private func configureStatusBarItem() {
+        statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        configureStatusBarButton(statusBarItem.button)
+    }
+
+    private func configureStatusBarButton(_ button: NSStatusBarButton?) {
+        guard let button = button else { return }
+        button.image = NSImage(named: Constants.statusBarIconName)
+        button.action = #selector(toggleChatPopover(_:))
+    }
+
+    private func registerServicesAndHotkeys() {
         NSApp.servicesProvider = self
         NSUpdateDynamicServices()
-        
         PermissionsManager.shared.requestScreenRecordingPermission()
-        
-        print("Registering Hotkey")
-        HotkeyManager.shared.registerHotkey(with: (key: UInt32(kVK_ANSI_6), modifiers: UInt32(shiftKey | cmdKey)), id: 1) {
-            print("Hotkey Cmd+Shift+6")
-            // Hotkey functionality
+        registerHotkey()
+    }
+
+    private func registerHotkey() {
+        HotkeyManager.shared.registerHotkey(with: (key: UInt32(Constants.hotkeyKey), modifiers: UInt32(Constants.hotkeyModifiers)), id: 1) {
             self.handleHotkeyPress()
         }
     }
 
     private func handleHotkeyPress() {
-        ScreenshotManager.shared.takeScreenshot { url in
-            guard let screenshotURL = url else { return }
-            OCRManager.shared.performOCR(on: screenshotURL) { text in
-                guard let recognizedText = text else { return }
-                print(recognizedText)
-                // Do something with the recognized text, e.g., copy to clipboard
-                self.copyTextToClipboard(text: recognizedText)
-            }
+        ScreenshotManager.shared.takeScreenshot { [weak self] url in
+            guard let self = self, let screenshotURL = url else { return }
+            self.performOCR(on: screenshotURL)
+        }
+    }
+
+    private func performOCR(on url: URL) {
+        OCRManager.shared.performOCR(on: url) { [weak self] text in
+            guard let self = self, let recognizedText = text else { return }
+            self.copyTextToClipboard(text: recognizedText)
         }
     }
 
@@ -65,47 +78,36 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let pasteboard = NSPasteboard.general
         pasteboard.clearContents()
         pasteboard.setString(text, forType: .string)
+        updateSharedTextModel(with: text)
+    }
+
+    private func updateSharedTextModel(with text: String) {
         DispatchQueue.main.async {
             self.sharedTextModel.inputText = text
-            self.showPopover()
+            self.showPopoverIfNeeded()
         }
     }
 
     @objc func processTextService(_ pboard: NSPasteboard, userData: String?, error: UnsafeMutablePointer<NSString>) {
-        print("Service 'Check with GPT' invoked")
         guard let selectedText = pboard.string(forType: .string) else {
-            print("Error: No text found")
             error.pointee = "No text was found." as NSString
             return
         }
-        
-        print("Selected Text: \(selectedText)")
-        
-        DispatchQueue.main.async {
-            self.sharedTextModel.inputText = selectedText
-            self.showPopover()
-        }
+        updateSharedTextModel(with: selectedText)
     }
 
-    private func showPopover() {
-        if let button = statusBarItem.button {
-            NSApp.activate(ignoringOtherApps: true)
-            if !popover.isShown {
-                popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
-            }
-        }
+    private func showPopoverIfNeeded() {
+        guard let button = statusBarItem.button, !popover.isShown else { return }
+        NSApp.activate(ignoringOtherApps: true)
+        popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
     }
-    
+
     @objc func toggleChatPopover(_ sender: AnyObject?) {
         if popover.isShown {
-           popover.performClose(sender)
+            popover.performClose(sender)
         } else {
-            if let button = statusBarItem.button {
-                NSApp.activate(ignoringOtherApps: true)
-                popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
-            }
+            showPopoverIfNeeded()
         }
     }
-    
-    
 }
+
